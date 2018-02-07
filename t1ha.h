082305ddea9751140bec5c2eca2b3410ffa1cc23
Leo Yuriev <leo@yuriev.ru>
2018-02-07 22:34:56 +0300

    t1ha: HNY!

--- t1ha.h
+++ t1ha.h
@@ -1,8 +1,8 @@
 /*
- *  Copyright (c) 2016-2017 Positive Technologies, https://www.ptsecurity.com,
+ *  Copyright (c) 2016-2018 Positive Technologies, https://www.ptsecurity.com,
  *  Fast Positive Hash.
  *
- *  Portions Copyright (c) 2010-2017 Leonid Yuriev <leo@yuriev.ru>,
+ *  Portions Copyright (c) 2010-2018 Leonid Yuriev <leo@yuriev.ru>,
  *  The 1Hippeus project (t1h).
  *
  *  This software is provided 'as-is', without any express or implied

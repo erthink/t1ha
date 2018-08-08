548c4925c6111ccc53143a4f5ec6cd30f7a7029f
Leonid Yuriev <leo@yuriev.ru>
2018-08-08 23:04:50 +0300

    t1ha: bump version to 2.0.4 (preparing for release).

--- t1ha.h
+++ t1ha.h
@@ -169,7 +169,7 @@
 
 #define T1HA_VERSION_MAJOR 2
 #define T1HA_VERSION_MINOR 0
-#define T1HA_VERSION_RELEASE 2
+#define T1HA_VERSION_RELEASE 4
 
 #ifndef __has_attribute
 #define __has_attribute(x) (0)
